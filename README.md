c1d0381aecf233918085a243ca1103aed932ee2e
JMarts32 <j.santosa@uniandes.edu.co>
2023-02-11 19:33:26 -0500

    Creado el modelo del fetch para seguir avanzando

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PcTWEB
 Repositorio de los ejercicios de web
+
+Resolveremos ejercicios en JavaScript para ir aprendiendo durante el semestre.
---
+++ asyncronia.js
@@ -0,0 +1,11 @@
+const URL1 = "monda";
+const URL2 = "monda";
+
+fetch(URL1).then(res => res.json()).then(res =>{
+    // llamo a la funcion para leer los datos
+
+
+})
+
+// Aca leo los datos
+function leer(){}
